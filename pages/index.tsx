55da2634c7a3ad989fc0b39bd897b61fc8c824fd
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-15 16:11:18 +0300

    Add Pancake recipe

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,13 +25,34 @@ export default function Index() {
       </header>
 
       <main>
-        <h2 className="text-center pt-10 pb-5 text-3xl">Biftek Kızartma</h2>
+        <h2 className="text-center pt-10 pb-5 text-3xl">Pankek</h2>
         <h3 className="text-center text-xl">Malzemeler</h3>
         <BsEggFried className="m-auto my-10" size={32} />
         <ol className="text-center">
-          <li>Biftek eti</li>
-          <li>Zeytinyağı</li>
+          <li>Beyaz peynir</li>
+          <li>Yarım demet Maydanoz</li>
+          <li>4 Yumurta</li>
+          <li>Un</li>
+          <li>Süt</li>
+          <li>Tuz</li>
+          <li>Tereyağ</li>
         </ol>
+        <h3 className="text-center text-xl">Hazırlanışı</h3>
+        <ol>
+          <li>Peyniri ezin.</li>
+          <li>Maydanozları ince ince doğrayın.</li>
+          <li>Peyniri maydanozla karıştırın.</li>
+          <li>Bir kapta yumurta çırpın.</li>
+          <li>
+            Yumurtanın içine bir bardak süt, peynirle maydanoz, un, tuz hepsini
+            koyup karıştırın.
+          </li>
+          <li>Tavayı tereyağ ile yağlayın.</li>
+          <li>
+            Karışımdan bir küçük kepçe kadar dökün ve iki yüzünü de pişirin.
+          </li>
+        </ol>
+        Bon Appétit
       </main>
 
       <footer className="my-10 text-right mx-5">
