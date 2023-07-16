4796d9d91a44e96ec488f890d557ad46d751fe4a
Gökay Gültekin <58526116+naphteine@users.noreply.github.com>
2023-07-16 20:37:29 +0300

    Basic recipe page

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
 
-const postsDirectory = join(process.cwd(), '_posts')
+const postsDirectory = join(process.cwd(), '_recipes')
 
 export function getPostSlugs() {
   return fs.readdirSync(postsDirectory)
---
+++ pages/meal/[slug].tsx
@@ -0,0 +1,82 @@
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
+import Head from "next/head";
+import markdownToHtml from "../../lib/markdownToHtml";
+import type PostType from "../../interfaces/post";
+
+type Props = {
+  post: PostType;
+  morePosts: PostType[];
+  preview?: boolean;
+};
+
+export default function Post({ post, morePosts, preview }: Props) {
+  const router = useRouter();
+  const title = `${post.title}`;
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <>
+      {router.isFallback ? (
+        <h1>Boss we're sleeping</h1>
+      ) : (
+        <>
+          <article className="mb-32">
+            <Head>
+              <title>{title}</title>
+            </Head>
+
+            <h1>{post.title}</h1>
+            <h2>{post.date}</h2>
+            <p>{post.ingredients}</p>
+
+            <p>{post.content}</p>
+          </article>
+        </>
+      )}
+    </>
+  );
+}
+
+type Params = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function getStaticProps({ params }: Params) {
+  const post = getPostBySlug(params.slug, [
+    "title",
+    "date",
+    "slug",
+    "content",
+    "ingredients",
+  ]);
+  const content = await markdownToHtml(post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(["slug"]);
+
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
